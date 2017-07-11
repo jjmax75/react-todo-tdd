dacd6074a018ee4ae0814e7f2e67aca814ce1aa9
John Behan <behanj@gmail.com>
2017-07-11 16:11:31 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react", "jsx-a11y", "import"
+  ],
+  "rules": {
+    "react/jsx-filename-extensions": [ 1, { "extensions": [ ".js", ".jsx" ] } ]
+  }
+};
