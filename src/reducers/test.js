3bbaf37ad8ae9632c749cfde626df4778ec64acf
John Behan <behanj@gmail.com>
2017-07-13 22:09:57 +0700

    tests and reducer

---
+++ src/reducers/test.js
@@ -0,0 +1,33 @@
+/* global it, expect, describe */
+
+import types from './../constants';
+import { reducer, initialState } from '.';
+
+describe('Reducer', () => {
+  const todoText = 'A Todo';
+
+  it('Should return the initial state when no action passed', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe('Submit todo', () => {
+    it('Should return the correct state', () => {
+      const action = {
+        type: types.SUBMIT_TODO,
+        id: 1,
+        text: todoText,
+      };
+
+      const expectedState = {
+        todos: [
+          {
+            id: 1,
+            text: todoText,
+          },
+        ],
+      };
+
+      expect(reducer(undefined, action)).toEqual(expectedState);
+    });
+  });
+});
