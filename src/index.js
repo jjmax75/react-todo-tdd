718204f16aaa2b46cbefa81657198ff64a56bb0c
John Behan <behanj@gmail.com>
2017-07-14 16:03:31 +0700

    remove comments, fix eslint warning

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+/* eslint-disable import/no-named-as-default */
 import App from './App';
+/* eslint-enable */
 import store from './store';
 
 ReactDOM.render(
