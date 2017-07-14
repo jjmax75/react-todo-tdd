5a9bd8723fed72a57ed593bee4982dd1d82815a6
John Behan <behanj@gmail.com>
2017-07-14 12:37:34 +0700

    setup todolist component

---
+++ src/components/todoList/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TodoList = (props) => {
+  const todoItems = (todos) => {
+    todos.map(todo => (
+      <li key={todo.id}>
+        <span className='todo-text'>{todo.text}</span>
+      </li>
+    ));
+  };
+
+  return (
+    <ul>
+      {todoItems(props.todos)}
+    </ul>
+  );
+};
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape(
+    {
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    },
+  )).isRequired,
+};
+
+export default TodoList;
